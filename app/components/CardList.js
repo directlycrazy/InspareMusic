893e6b29518fab558a4b341485b37a30d0af24e3
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2023-11-03 15:23:50 -0400

    Change card layout

--- app/components/CardList.js
+++ app/components/CardList.js
@@ -1,6 +1,6 @@
 export default function CardList({ children }) {
 	return (
-		<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 mt-5">
+		<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-5">
 			{children}
 		</div>
 	);
